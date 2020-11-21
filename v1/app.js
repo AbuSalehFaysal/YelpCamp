9cd0e4280e9b03fe96c3587650dd8dabc8ffaeca
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2020-11-22 03:33:56 +0600

    new campground form add

--- v1/app.js
+++ v1/app.js
@@ -1,19 +1,34 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+    {name: "Shahriar Kabir", image: "https://images.pexels.com/photos/2108709/pexels-photo-2108709.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {name: "Mohammad Shuvo", image: "https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {name: "Zannatun Nayeem", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350"},
+];
 
 app.get("/", function(req, res){
     res.render("landing")
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "Shahriar Kabir", image: "https://images.pexels.com/photos/2108709/pexels-photo-2108709.jpeg?auto=compress&cs=tinysrgb&h=350"},
-        {name: "Mohammad Shuvo", image: "https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350"},
-        {name: "Zannatun Nayeem", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    ]
     res.render("campgrounds",{campgrounds: campgrounds});
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new");
+});
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
 });
 
 app.listen(3000, function(){
