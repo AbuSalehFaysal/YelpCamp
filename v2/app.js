e8348d5db3ecb493fc39d233508c4a55cec99930
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2020-11-30 02:21:38 +0600

    v2 - adding mongoose, create Schema, model

--- v2/app.js
+++ v2/app.js
@@ -1,70 +1,62 @@
-const express    = require("express");
-      app        = express();
-      bodyParser = require("body-parser");
-      mongoose   = require("mongoose");
+const express = require("express");
+const  app = express();
+const  bodyParser = require("body-parser");
+const  mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/db_yelpCamp", {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost/yelp_camp");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String
-});
+  name: String,
+  image: String
+}); 
+
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-//DATABASE MANUPULATION
-// Campground.create(
-//     {
-//         name: "Rangamati",
-//         image: "https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350"
-//     }, function(err, campground){
-//         if(err){
-//             console.log(err);
-//         } else{
-//             console.log("Campground Added!!!");
-//             console.log(campground);
-//         }
-//     });
-
-app.get("/", function(req, res){
-    res.render("landing")
+app.get("/", function (req, res) {
+  res.render("landing");
 });
 
-app.get("/campgrounds", function(req, res){
-    //GET ALL CAMPGROUNDS
-    Campground.find({},function(err, allCampgrounds){
-        if (err){
-            console.log(err);
-        } else {
-            res.render("campgrounds",{campgrounds: allCampgrounds});
-        }
-    });
-    // res.render("campgrounds",{campgrounds: campgrounds});
+app.get("/campgrounds", function (req, res) {
+  //GET ALL CAMPGROUND FROM DB
+  Campground.find({}, function(err, allcampgrounds){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("campgrounds", { campgrounds: allcampgrounds });
+    }
+  });
 });
 
-app.get("/campgrounds/new", function(req, res){
-    res.render("new");
+app.get("/campgrounds/new", function (req, res) {
+  res.render("new");
 });
 
-app.post("/campgrounds", function(req, res){
-    var name = req.body.name;
-    var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    //CREATE A NEW CAMPGROUND AND SAVE IT TO DATABASE
-    Campground.create(newCampground, function(err, newlyCreated){
-        if (err) {
-            console.log(err);
-        } else {
-            res.redirect("/campgrounds");
-        }
-    });
+app.post("/campgrounds", function (req, res) {
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = { name: name, image: image };
+  // campgrounds.push(newCampground);
+  //CREATE A NEW CAMPGROUND and SAVE TO DB
+  Campground.create(newCampground, function(err, newlyCreated){
+    if(err){
+      console.log(err);
+    }else{
+      console.log("Successfully added!");
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
-app.listen(4000, function(){
-    console.log("Server Has Started!!!");
+app.listen(3000, function () {
+  console.log("Server Has Started!!!");
 });
 
 // app.listen(process.env.PORT, process.env.IP);
